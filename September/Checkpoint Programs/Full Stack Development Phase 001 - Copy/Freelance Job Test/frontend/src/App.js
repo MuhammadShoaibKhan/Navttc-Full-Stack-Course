8ff4a4287dae35473b0ca5c469f85a39b5f30a5b
Muhammad Shoaib Khan <shoaibmuhammad960@msn.com>
2024-09-24 13:10:07 +0500

    Data Copy Added

---
+++ September/Checkpoint Programs/Full Stack Development Phase 001 - Copy/Freelance Job Test/frontend/src/App.js
@@ -0,0 +1,62 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Ali from "./pages/Ali";
+import Friends from "./pages/Friends";
+import BeMyFriend from "./pages/BeMyFriend";
+import React, { useEffect, useState } from "react";
+import "./style.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./component/Header";
+import Login from "./pages/Login";
+import { Signup } from "./pages/Signup";
+
+export const Context = React.createContext();
+
+function App() {
+  const [friends, setFriends] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [login, setLogin] = useState(false);
+
+  useEffect(() => {
+    try {
+      const getFriends = async () => {
+        const res = await fetch("http://localhost:5000/friend");
+        const resData = await res.json();
+        setFriends(resData);
+      };
+      getFriends();
+    } catch (err) {
+      console.log(err);
+      setLoading(false);
+    }
+    setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+  }, []);
+
+  return (
+    <Context.Provider value={[login, setLogin]}>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Ali />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route
+            path="/friends"
+            element={<Friends friends={friends} loading={loading} />}
+          />
+          <Route
+            path="/bemyfriend"
+            element={<BeMyFriend loading={loading} />}
+          />
+          <Route
+            path="/bemyfriend/:id"
+            element={<BeMyFriend friends={friends} loading={loading} />}
+          />
+        </Routes>
+      </BrowserRouter>
+    </Context.Provider>
+  );
+}
+
+export default App;
---
+++ September/Checkpoint Programs/Full Stack Development Phase 001 - Copy/Freelance Job Test/frontend/src/pages/Ali.jsx
@@ -0,0 +1,15 @@
+import { Link } from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
+function Ali (){
+    return(
+        <Container>
+       <div className='mt-5'>
+        {/* <img src={ali} alt='ali' /> */}
+        <h1>Ali Raza</h1>
+        <h2>Frontend Developer</h2>
+        <p>Contact: 03xxxxxxxxx</p>
+       </div>
+       </Container>
+    )
+}
+export default Ali;
